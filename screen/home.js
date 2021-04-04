3de1ab13f8adfdb6e2354f4d692a962aa8f9abd0
Defaroyan <defaroyan@student.uns.ac.id>
2021-04-04 21:39:40 +0700

    adding Modal

--- screen/home.js
+++ screen/home.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState} from 'react';
-import {Text, View, FlatList, TouchableOpacity } from 'react-native';
+import {StyleSheet, Text, View, FlatList, TouchableOpacity, Modal } from 'react-native';
 import { global } from '../styles/global'
 import Card from '../shared/cards'
+import { MaterialIcons } from '@expo/vector-icons'
 
 export default function Home({navigation}) { //Passibng props.navigation can use the (props) but this is a destructuring
-
+  const[modal, setModal] = useState(false);
   const [review,setReviews] = useState([
     {title: 'Zeet, The Zeeter', rating : 5, body: 'zezezezeze', key:1},
     {title: 'Beet, The Burglar', rating : 4, body: 'bebebebebe', key:2},
@@ -16,6 +17,16 @@ export default function Home({navigation}) { //Passibng props.navigation can use
 
   return (
     <View style={global.container}>
+      <Modal visible={modal} animationType='slide'>
+        <View style={styles.modal}>
+          {/*<MaterialIcons style={{...styles.modalToggle, ...styles.modalClose}} name="close" size={24} onPress={()=>setModal(false)}/>*/}
+          <MaterialIcons style={[styles.modalToggle, styles.modalClose]} name="close" size={24} onPress={()=>setModal(false)}/>
+          <Text> MODALITY </Text>
+        </View>
+      </Modal>
+
+      <MaterialIcons style={styles.modalToggle} name="add" size={24} onPress={()=>setModal(true)}/>
+
       <StatusBar style="auto" />
       <FlatList data={review} renderItem={({item}) =>(
         <TouchableOpacity onPress={() => navigation.navigate('Ripiu', item)}>
@@ -27,3 +38,21 @@ export default function Home({navigation}) { //Passibng props.navigation can use
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  modal:{
+    flex:1,
+  },
+  modalToggle:{
+    marginBottom:10,
+    borderWidth:1,
+    borderColor: "#008891",
+    padding:10,
+    borderRadius:10,
+    alignSelf:'center'
+  },
+  modalClose:{
+    marginTop:20,
+    marginBottom: 0,
+  }
+})
